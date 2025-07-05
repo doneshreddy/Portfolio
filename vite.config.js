eb3b44f51cefbd90241ca4450b2a7429dcef9e6d
doneshreddy <126519861+doneshreddy@users.noreply.github.com>
2025-07-05 03:39:08 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/React-Portfolio/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    // ... other build options like rollupOptions if you need them
+  },
 });
